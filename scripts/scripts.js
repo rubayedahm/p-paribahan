5a8a807d7c2919d7abe31d11688d68aa6a50a74d
Rubayed Ahmed <rubayed.ahmed02@gmail.com>
2024-08-16 22:20:30 +0600

    total price added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,38 @@
-const seats = document.querySelectorAll('td');
+const seats = document.querySelectorAll("td");
 let selectedSeats = [];
 
-
-seats.forEach(seat => {
-    seat.addEventListener('click', () => {
-      if (selectedSeats.length < 4) {
-        seat.style.backgroundColor = '#1DD100';
-        selectedSeats.push(seat.textContent); // Store seat number
-        updateSelectedSeatsDisplay()
-      } else {
-        alert('You can only select 4 seats');
-      }
-    });
+seats.forEach((seat) => {
+  seat.addEventListener("click", () => {
+    if (selectedSeats.length < 4) {
+      seat.style.backgroundColor = "#1DD100";
+      selectedSeats.push(seat.textContent); // Store seat number
+      updateSelectedSeatsDisplay();
+    } else {
+      alert("You can only select 4 seats");
+    }
   });
+});
 
-  function updateSelectedSeatsDisplay() {
-    const selectedSeatsSpan = document.getElementById('seat-count');
-    selectedSeatsSpan.textContent = selectedSeats.length;
-  }
+function updateSelectedSeatsDisplay() {
+  const selectedSeatsSpan = document.getElementById("seat-count");
+  selectedSeatsSpan.textContent = selectedSeats.length;
+
+  const totalTicketPriceElement = document.getElementById("total-ticket-price");
+ 
   
-  function getSelectedSeatNumbers() {
-    return selectedSeats;
-  }
-  
-  
+  const totalPrice = selectedSeats.length * 550;
+  totalTicketPriceElement.textContent = totalPrice
+
+  console.log(totalTicketPriceValue, totalPrice)
+}
+
+function getSelectedSeatNumbers() {
+  return selectedSeats;
+}
+
+// Total Ticket Price
+const totalTicketPriceElement = document.getElementById("total-ticket-price");
+const totalTicketPriceValue = totalTicketPriceElement.innerText;
+
+const totalPrice = selectedSeats.length * 550;
+console.log(totalPrice);
